357148350ee7e8318881ed04f37fa7e1a7d00219
Matteo Giachino <matteog@gmail.com>
2014-07-01 00:56:51 +0200

    source, destination and rotation ad command line args

--- rotate.go
+++ rotate.go
@@ -10,6 +10,25 @@ import (
     "code.google.com/p/graphics-go/graphics"
 )
 
+var rotation string
+
+func main() {
+	fName, fDest, rotation := extract_args()
+	fmt.Printf("rotating %s by %f. destination %s\n", fName, rotation, fDest)
+
+	fImg, _ := os.Open(fName)
+	defer fImg.Close()		
+	width, height := get_size(fName)
+	img, _, _ := image.Decode(fImg)
+	m := image.NewRGBA(image.Rect(0, 0, height, width))
+	graphics.Rotate(m, img, &graphics.RotateOptions{rotation})
+
+	toimg, _ := os.Create(fDest)
+	defer toimg.Close()
+
+	jpeg.Encode(toimg, m, &jpeg.Options{jpeg.DefaultQuality})
+}
+
 func get_size(fName string) (int, int) {
 	fImgSize, _ := os.Open(fName)
 	defer fImgSize.Close()
@@ -21,19 +40,24 @@ func get_size(fName string) (int, int) {
 	return imgConfig.Width, imgConfig.Height
 }
 
-func main() {
-    args := os.Args[1:]
-	for _, fName := range args {
-		fImg, _ := os.Open(fName)
-		defer fImg.Close()		
-		width, height := get_size(fName)
-		img, _, _ := image.Decode(fImg)
-		m := image.NewRGBA(image.Rect(0, 0, height, width))
-		graphics.Rotate(m, img, &graphics.RotateOptions{1.57079633})
-
-		toimg, _ := os.Create("new.jpg")
-		defer toimg.Close()
-
-		jpeg.Encode(toimg, m, &jpeg.Options{jpeg.DefaultQuality})
+func extract_args() (string, string, float64) {
+	args := os.Args[1:]
+	if len(args) < 2 {
+		fmt.Println("2 args required: image source, destination and rotation")
+		os.Exit(1)
 	}
+	if len(args) > 4 {
+		fmt.Println("Too many arguments, required: image source, destination and rotation")
+		os.Exit(1)
+	}
+	fName := args[0]
+    fDest := args[1]
+    rotation := args[2]
+    var rotationAngle float64 = 0
+    if rotation == "r" {
+    	rotationAngle = 1.57079633
+    } else {
+    	rotationAngle = 4.71238899
+    }
+    return fName, fDest, rotationAngle
 }
